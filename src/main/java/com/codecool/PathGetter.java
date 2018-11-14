f618ddd8705662de611d888d601dcc68e0e71b73
reggae-krk <monika.gorecka.agh@gmail.com>
2018-11-14 11:50:07 +0100

    Add getPaths method

--- src/main/java/com/codecool/PathGetter.java
+++ src/main/java/com/codecool/PathGetter.java
@@ -1,4 +1,27 @@
 package com.codecool;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class PathGetter {
+
+    public List<String> getPaths() {
+        List<String> paths = new ArrayList<>();
+        try {
+            Stream<Path> pathsStream = Files.walk(Paths.get("src/main/resources"));
+            paths = pathsStream.filter(Files::isRegularFile)
+                    .map(x -> x.toString())
+                    .collect(Collectors.toList());
+        }
+        catch (IOException e) {
+            System.out.println("There is no files in the folder");
+        }
+        return paths;
+    }
 }
