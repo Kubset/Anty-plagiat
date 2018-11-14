d53bb76f58b8eb4df415120185f981e02bb63eb0
Kubset <kuba1228334@gmail.com>
2018-11-14 14:01:07 +0100

    add example data

---
+++ src/main/java/com/codecool/PlagiatService.java
@@ -0,0 +1,14 @@
+package com.codecool;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class App {
+
+
+
+
+
+}
