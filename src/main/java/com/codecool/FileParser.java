dd34c955624186b225952ffb6f494d0a84d7762a
reggae-krk <monika.gorecka.agh@gmail.com>
2018-11-14 10:28:27 +0100

    Add convert method

--- src/main/java/com/codecool/FileParser.java
+++ src/main/java/com/codecool/FileParser.java
@@ -1,4 +1,23 @@
 package com.codecool;
 
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FileParser {
+
+    private Charset charset = StandardCharsets.UTF_8;
+
+    private String convert(String path) throws IOException {
+
+        InputStream inputStream = new FileInputStream(path);
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, charset));
+        String fileToString = br.lines().collect(Collectors.joining(System.lineSeparator()));
+        inputStream.close();
+        return fileToString;
+    }
+
 }
