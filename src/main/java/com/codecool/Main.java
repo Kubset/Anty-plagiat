4ba54ad680b33afb8b9a94b4d5d426e2be09eb7f
Kubset <kuba1228334@gmail.com>
2018-11-14 14:02:18 +0100

    add main application logic

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -1,13 +1,37 @@
 package com.codecool;
 
+import java.io.File;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
+
 /**
  * Hello world!
  *
  */
 public class Main
 {
-    public static void main( String[] args )
+    private final static String USER_PATH = "src/main/userFiles/";
+    public static void main( String[] args ) throws InterruptedException,ExecutionException
     {
-        System.out.println( "Hello World!" );
+        PlagiatService plagiatService = null;
+
+        Scanner sc = new Scanner(System.in);
+        String[] inputs = sc.nextLine().split(" ");
+
+
+
+        if(inputs.length == 2) {
+            plagiatService = new PlagiatService(USER_PATH + inputs[0], Integer.valueOf(inputs[1])) ;
+        } else if(inputs.length == 1) {
+            plagiatService = new PlagiatService(inputs[0]) ;
+        }
+        if(plagiatService != null) {
+            System.out.println(plagiatService.toString());
+        }
     }
+
 }
